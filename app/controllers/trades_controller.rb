6da13d1225449aea8d47012fd7ca175b0bb6daf8
yasmin <yasmin@yasminguimaraes.com>
2022-04-20 07:48:35 -0300

    fixing to let just rescue block

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,17 +2,14 @@ class TradesController < ApplicationController
   before_action :authorize_request
 
   def show
-    begin
-      render json: {trade: Trade.find(params[:id])}
-    rescue => e
-      render json: {errors: e.message}, status: :not_found
-    end
+    render json: {trade: Trade.find(params[:id])}
+    rescue ActiveRecord::RecordNotFound
+      render json: {errors: "Couldn't find Trade with 'id'=#{params[:id]}"}, status: :not_found
   end
 
   def edit
     begin
       trade = Trade.find(params[:id])
-      puts trade.state
       if trade.state == 'pending'
         trade.update!(state: trade_params.to_i)
         render json: {message: 'Trade status updated!'}
